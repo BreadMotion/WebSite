2cea31089a4bb4a600ee9d1c36baf48c81b80358
y-yamasaki <yamazakimotionpg@gmail.com>
2024-10-11 14:55:36 +0900

    バグ修正

--- js/sendContact.js
+++ js/sendContact.js
@@ -1,8 +1,8 @@
 function getform() {
   mailAddress = document.contentForm.address.value;
-  var URL = "https://breadmotion.github.io/WebSite/Contact/contact.html";
+  var URL = "https://breadmotion.github.io/WebSite/Contact/contact.html?";
   var param = "address=" + mailAddress;
 
   alert("send " + URL + param);
-  location.href = URL = param;
+  location.href = URL + param;
 }
