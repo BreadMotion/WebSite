901485e05b2b345561dc2f6a1cdb23ca0dde1c90
BreadMotion <89498861+BreadMotion@users.noreply.github.com>
2024-10-12 04:50:24 +0900

    ブログテンプレート作成

--- js/sendContact.js
+++
@@ -1,8 +0,0 @@
-function getform() {
-  mailAddress = document.contentForm.address.value;
-  var URL = "https://breadmotion.github.io/WebSite/Contact/contact.html?";
-  var param = "address=" + mailAddress;
-
-  alert("send " + URL + param);
-  location.href = URL + param;
-}
