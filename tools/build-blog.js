602d87ba99e18a3ad7eafb4935c8a03e5d55224d
BreadMotion <89498861+BreadMotion@users.noreply.github.com>
2025-11-22 21:11:29 +0900

    email metaの設定

--- tools/build-blog.js
+++ tools/build-blog.js
@@ -73,6 +73,7 @@ function createHtml({
     <meta property="og:type" content="blog" />
     <meta property="og:image" content="../assets/img/ogp.png" />
     <meta property="og:site_name" content="PanKUN" />
+    <meta property="og:email" content="pankun.dev@gmail.com" />
 
     <link rel="stylesheet" href="../assets/css/base.css" />
     <link rel="stylesheet" href="../assets/css/layout.css" />
