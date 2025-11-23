ac7a9239277a2848256cafc9971d192552fdc515
BreadMotion <89498861+BreadMotion@users.noreply.github.com>
2025-11-24 00:08:29 +0900

    キャンバス調整

--- assets/js/menuAnimation.js
+++ assets/js/menuAnimation.js
@@ -1,4 +1,6 @@
 let animationCanvas;
+let circleSize = 0;
+let growing = true;
 
 function setup() {
   const parent = document.getElementById(
@@ -9,13 +11,29 @@ function setup() {
     parent.offsetHeight,
   );
   animationCanvas.parent("menuAnimationCanvas");
-  background(22, 27, 34);
+  background(22, 27, 34); // 背景色を一度だけ設定
 }
 
 function draw() {
+  background(22, 27, 34, 50);
   noStroke();
-  fill(255, 255, 255, 50);
-  ellipse(random(width), random(height), 10, 10);
+  fill(255, 255, 255, 150);
+
+  if (growing) {
+    circleSize += 0.5;
+    if (circleSize > 50) {
+      growing = false;
+    }
+  } else {
+    circleSize -= 0.5;
+    if (circleSize < 10) {
+      growing = true;
+    }
+  }
+  ellipse(width / 2, height / 2, circleSize, circleSize);
+
+  fill(255, 255, 255, 80);
+  ellipse(mouseX, mouseY, 15, 15);
 }
 
 function windowResized() {
