59c5ed9d25dd8167a152ce418bbe01b6a8f89897
BreadMotion <89498861+BreadMotion@users.noreply.github.com>
2025-11-24 02:47:42 +0900

    パーティクルアニメーション追加

--- assets/js/menuAnimation.js
+++ assets/js/menuAnimation.js
@@ -1,13 +1,16 @@
 let animationCanvas;
 let particles = [];
-const numParticles = 100;
+const numParticles = 1000;
 const maxParticleSize = 8;
 const minParticleSize = 2;
-const maxSpeed = 2;
-const repulsionRadius = 150;
-const repulsionStrength = 1;
+const maxSpeed = 10;
+const repulsionRadius = 250;
+const repulsionStrength = 20;
+
+let isMouseInsideBrowserWindow = false;
 
 function setup() {
+  // ページの全体の高さを取得
   let pageHeight = Math.max(
     document.body.scrollHeight,
     document.documentElement.scrollHeight,
@@ -51,46 +54,60 @@ function windowResized() {
 class Particle {
   constructor() {
     this.pos = createVector(random(width), random(height));
-    this.vel = p5.Vector.random2D().mult(
-      random(0.5, maxSpeed),
-    );
+    this.origPos = createVector(this.pos.x, this.pos.y);
+    this.vel = p5.Vector.random2D().mult(random(0.1, 1));
     this.acc = createVector(0, 0);
     this.size = random(minParticleSize, maxParticleSize);
-    this.color = color(255, 255, 255, random(50, 150)); // 半透明の白
+    this.color = color(255, 255, 255, random(50, 150));
+    this.mass = random(0.5, 1.5);
   }
 
   applyForce(force) {
-    this.acc.add(force);
+    let f = p5.Vector.div(force, this.mass);
+    this.acc.add(f);
   }
 
   update() {
-    let mouse = createVector(mouseX, mouseY);
-    let force = p5.Vector.sub(this.pos, mouse);
-    let distance = force.mag();
+    if (isMouseInsideBrowserWindow) {
+      let mouse = createVector(mouseX, mouseY);
+      let force = p5.Vector.sub(this.pos, mouse);
+      let distance = force.mag();
+      let toOrigPos = p5.Vector.sub(this.origPos, this.pos);
+      this.vel.add(toOrigPos.mult(0.0001));
+      if (distance < repulsionRadius) {
+        distance = max(distance, 5);
+        let strength = -repulsionStrength / distance;
+        force.setMag(strength);
+        this.applyForce(force);
+      }
 
-    if (distance < repulsionRadius) {
-      let strength =
-        -repulsionStrength / (distance * distance);
-      force.setMag(strength);
-      this.applyForce(force);
+      this.vel.add(this.acc);
+      this.vel.mult(0.97);
+      this.vel.limit(maxSpeed);
     }
-
-    this.vel.add(this.acc);
-    this.vel.limit(maxSpeed);
     this.pos.add(this.vel);
     this.acc.mult(0);
 
-    if (this.pos.x > width || this.pos.x < 0) {
+    if (
+      this.pos.x > width - this.size / 2 ||
+      this.pos.x < this.size / 2
+    ) {
       this.vel.x *= -1;
+      if (this.pos.x > width - this.size / 2)
+        this.pos.x = width - this.size / 2;
+      if (this.pos.x < this.size / 2)
+        this.pos.x = this.size / 2;
     }
-    if (this.pos.y > height || this.pos.y < 0) {
+    if (
+      this.pos.y > height - this.size / 2 ||
+      this.pos.y < this.size / 2
+    ) {
       this.vel.y *= -1;
+      if (this.pos.y > height - this.size / 2)
+        this.pos.y = height - this.size / 2;
+      if (this.pos.y < this.size / 2)
+        this.pos.y = this.size / 2;
     }
-
-    if (this.pos.x > width) this.pos.x = 0;
-    if (this.pos.x < 0) this.pos.x = width;
-    if (this.pos.y > height) this.pos.y = 0;
-    if (this.pos.y < 0) this.pos.y = height;
   }
 
   display() {
@@ -99,3 +116,20 @@ class Particle {
     ellipse(this.pos.x, this.pos.y, this.size, this.size);
   }
 }
+
+window.addEventListener("load", () => {
+  document.body.addEventListener("mouseenter", () => {
+    isMouseInsideBrowserWindow = true;
+    console.log(
+      "Mouse entered body. isMouseInsideBrowserWindow:",
+      isMouseInsideBrowserWindow,
+    );
+  });
+  document.body.addEventListener("mouseleave", () => {
+    isMouseInsideBrowserWindow = false;
+    console.log(
+      "Mouse left body. isMouseInsideBrowserWindow:",
+      isMouseInsideBrowserWindow,
+    );
+  });
+});
